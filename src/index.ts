70a27d54d515c52fcfa083f99036eed285a1edfd
Sergio Mendieta <mendiets@uci.edu>
2024-09-22 20:07:00 -0700

    Added Post Route(Broken)
    Post is supposed to print the populated req.body(req.body is populated through the use of line 8 and 9 app.use functions) but it doesn't print the correct output

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,29 @@
 import express, { Express, Request, Response } from "express";
-import fileo from 'fs';
 import {GetUserId, GetUserData} from './sql/database.js'; // tsc creates error, doesnt include .js extension - because of ESM and node shit, just leave it like this with .js
+import bodyParser from "body-parser";
 
-
-const port: Number = Number(process.env.PORT) || 3000; // remove port later in dev
+const port: Number = Number(process.env.PORT) || 8000; // remove port later in dev
 const app: Express = express();
-app.use(express.static('public'));
-let start = 1;
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }))
+//app.use(express.json());
+//app.use(express.urlencoded({ extended: true }));
+//app.use(express.static('public')); Don't know why we need this
+//let start = 1; Don't know why we need this
 app.get("/", (req: Request, res: Response): void => {
+    console.log("Recieved request");
     // res.render('index');
 });
 
+app.post("/registeruser", (req: any, res: Response): void => {
+    res.set('Access-Control-Allow-Origin', '*');
+    console.log(req.body);
+    res.send("Request Received");
+});
 // register route, does it need any info? or will use the request body
-app.put("/register", async (req: Request, res: Response): Promise<void> => {
+app.put("/", async (req: Request, res: Response): Promise<void> => {
+    res.set('Access-Control-Allow-Origin', '*');
     // talk to mysql database and update the database
     // rerq body should give me the few items from the form
 
@@ -23,7 +33,7 @@ app.put("/register", async (req: Request, res: Response): Promise<void> => {
     // let email: String = req.params.email;
     // let major: String = req.params.major;
     // let student_id: String = req.params.id;
-    
+    console.log(first_name);
     let number_id = Number(student_id);
     if (isNaN(number_id)) {
         res.status(200).send('Success!'); 
