e4269bb621ea22b14a0e9f7bde48c109ff28053f
Sergio Mendieta <sergiboka03@gmail.com>
2025-03-05 19:25:27 -0800

    changed for server

--- src/sql/database.ts
+++ src/sql/database.ts
@@ -10,7 +10,7 @@ dotenv.config();
 const pool = mysql.createPool({  //You can go without the .promise(). If you initialize a pool without.promise(), you will have to rely on callback functions. 
     host: process.env.MYSQL_HOST,
     user: process.env.MYSQL_USER,
-    password: 'root',//process.env.MYSQL_PASSWORD,
+    password: process.env.MYSQL_USER,//process.env.MYSQL_PASSWORD,
     database: process.env.MYSQL_DATABASE,
     port: 3306, // Default MySQL port
     connectTimeout: 5000 // 5 seconds
